eeec5c35967ff198a164cd106a246a483b89b0ad
AzizAlbaqshi <eng.albaqshi@gmail.com>
2021-05-24 19:22:26 +0300

    Task 4

--- story.js
+++ story.js
@@ -2,4 +2,4 @@ let name = "aziz" ;
 let age = 32 ;
 let hobby = "football" ; 
 
-console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
+console.log(`Mr./Mrs. ${name}, in 33clear years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
